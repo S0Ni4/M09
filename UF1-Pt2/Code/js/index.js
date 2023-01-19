b1de9aa17400cb50f74fc4bfdd92e52a83dfb6ca
borunx <92715144+borunx@users.noreply.github.com>
2023-01-19 23:42:21 +0100

    Add files via upload

---
+++ UF1-Pt2/Code/js/index.js
@@ -0,0 +1,28 @@
+// Initialize tooltips
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    // Carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+
+});
